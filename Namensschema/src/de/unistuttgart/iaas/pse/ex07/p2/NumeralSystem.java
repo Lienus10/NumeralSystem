dcd6213b3481662212c9bb985258d3a16c0a0c6e
Lienus10 <langer.julius@gmx.de>
2016-12-13 17:33:48 +0100

    Soweit fertiges und lauffähiges Programm. Besitzt allerdings noch 'parseInt'

--- Namensschema/src/de/unistuttgart/iaas/pse/ex07/p2/NumeralSystem.java
+++ Namensschema/src/de/unistuttgart/iaas/pse/ex07/p2/NumeralSystem.java
@@ -1,46 +1,82 @@
 package de.unistuttgart.iaas.pse.ex07.p2;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
- * test
+ * Diese Klasse bietet Methoden, die das Umrechnen von Zahlen in den Zahlensystemen von 1 bis 16
+ * ermöglichen
  * 
- * 
- * testbranch
- * 
- * @author juliu
- *
+ * @author Langer, 3255917, st149962@stud.uni-stuttgart.de
+ * @author Zouboulis, 3230893, st148316@stud.uni-stuttgart.de
+ * @author Honecker, 2813091, st148147@stud.uni-stuttgart.de  
  */
 public class NumeralSystem {
 
 
 	public static void main(String[] args) {
-		//				System.out.println(toFinal(190, 12));
-		//				System.out.println(inZeichen(13));
-		printMenu();
+		printMenu();	
 	}
 
+	/**
+	 * Diese Methode liest die Nutzereingaben ein
+	 */
 	public static void printMenu(){
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		String dez;
-		int base1, base2;
+		int base1 = 0, base2 = 0;
 
 		//Die Nutzereingaben
+
+		System.out.println("welche Basis hat der Ausgangswert? ");
+		String s1 = scanner.nextLine();
+		if(s1.equals("0")){
+			System.exit(0);
+		}
+		if(!pruefeInputBase(s1)){
+			System.err.println("Bitte geben Sie einen gültigen Wert ein!");
+			printMenu();
+		}
+
+
 		System.out.println("Welchen Wert wollen Sie umrechnen? ");
 		dez = scanner.nextLine();
-
-		System.out.println("welche Basis hat der Wert? ");
-		base1 = scanner.nextInt();
+		dez = dez.toUpperCase();		
 
 		System.out.println("In welche Basis soll der Wert umgerechnet werden? ");
-		base2 = scanner.nextInt();
+		String s2 = scanner.nextLine();
 
-		System.out.println(zuweisung(dez, base1, base2));
-		//		System.out.println(conv2Dez(dez, base1, base2));
+		if(pruefeInputBase(s1) && pruefeInputBase(s2)){
+			base1 = Integer.parseInt(s1);
+			base2 = Integer.parseInt(s2);
+
+			if(pruefeInputDez(dez, base1, base2)){
+				System.out.println(zuweisung(dez, base1, base2));
+			} else{
+				System.err.println("Bitte geben Sie einen gültigen Wert ein!");
+				printMenu();
+			}
+		} else{
+
+			System.err.println("Bitte geben Sie einen gültigen Wert ein!");
+			printMenu();
+		}
 	}
 
 
+
+
+	/**
+	 * Diese Methode startet auf Grundlage der Nutzereingaben die
+	 * dementsprechenden Methoden an
+	 * 
+	 * @param dez (String) Nutzereingabe für die umzurechnende Zahl
+	 * @param base1 (int) Nutzereingabe für das Ausgangszahlensystem
+	 * @param base2 (int) Nutzereingabe für das Zielzahlensystem
+	 * @return (String) der errechnete Wert
+	 */
 	public static String zuweisung(String dez, int base1, int base2) {
 
 		if(base1 == base2){
@@ -58,6 +94,13 @@ public class NumeralSystem {
 		return toFinal(toDez(dez, base1), base2);
 	}
 
+	/**
+	 * Wandelt eine Zahl beliebigem Zahlensystems in das Dezimalsystem um
+	 * 
+	 * @param dez (String) Nutzereingabe für die umzurechnende Zahl
+	 * @param ersteBase (int) Nutzereingabe für das Ausgangszahlensystem
+	 * @return (int) der Wert im Dezimalsystem 
+	 */
 	public static int toDez(String dez, int ersteBase){
 
 		dez = umkehren(dez);
@@ -66,19 +109,24 @@ public class NumeralSystem {
 		int summe = 0;
 
 		for(int i = 0; i < dez.length(); i++){
-			if(Character.isLetter(dez.charAt(0))){
+			if(Character.isLetter(dez.charAt(i))){
 				t = inZahl(dez.charAt(i));
 			} else {
 				t = dez.charAt(i) - 48;
 			}			
 			summe = (int) (summe + (t * Math.pow(ersteBase, i)));
 		}
-
 		return summe;
 
 	}
 
-
+	/**
+	 * Wandelt eine Zahl aus dem Dezimalsystem in ein beliebiges Zahlensystem um
+	 * 
+	 * @param b (int) die Zahl im Dezimalsystem
+	 * @param zielBase (int) das Zielzahlensystem
+	 * @return (String) der Wert im Zielzahlensystem
+	 */
 	public static String toFinal(int b, int zielBase){
 
 		int h = -1;
@@ -100,7 +148,65 @@ public class NumeralSystem {
 
 	}
 
+	/**
+	 * Überprüft die Nutzereingabe des Ausgangswertes auf Richtigkeit
+	 * 
+	 * @param dez (String) Nutzereingabe für die umzurechnende Zahl
+	 * @param base1 (int) Nutzereingabe für das Ausgangszahlensystem
+	 * @param base2 (int) Nutzereingabe für das Zielzahlensystem
+	 * @return (boolean) Ist die Nutzeringabe korrekt
+	 */
+	public static boolean pruefeInputDez(String dez, int base1, int base2) {
 
+		char[] ch = dez.toCharArray();
+		Integer[] in = new Integer[dez.length()];
+
+		for(int i = 0; i < dez.length(); i++){
+			if(Character.isLetter(ch[i])){
+				in[i] = inZahl(dez.charAt(i));
+			} else{
+				in[i] = ch[i] - 48;
+			}
+		}
+		Arrays.sort(in);
+		Arrays.sort(in, Collections.reverseOrder());
+
+		if(in[0] < base1){
+			return true;
+		} else{
+			return false;
+		}
+	}
+
+
+	/**
+	 * Überprüft die Nutzereingabe der Basis auf Richtigkeit
+	 * 
+	 * @param s (String) Die Nutzereingabe 
+	 * @return (boolean) Ist die Nutzereingabe korrekt
+	 */
+	public static boolean pruefeInputBase(String s){
+
+		for(int i = 0; i < s.length(); i++){
+			if(Character.isLetter(s.charAt(0))){
+				return false;
+			}
+		}
+
+		if(Integer.parseInt(s) > 1 && Integer.parseInt(s) < 17){
+			return true;
+		} else{
+			return false;
+		}
+
+	}
+
+	/**
+	 * Dreht einen String um 
+	 * 
+	 * @param s (String) der umzuwandelnde String
+	 * @return (String) der umgewandelte String
+	 */
 	public static String umkehren(String s){
 
 		String s1 = "";
@@ -112,7 +218,12 @@ public class NumeralSystem {
 		return s1;
 	}
 
-
+	/**
+	 * Wandelt ein Zeichen in die entsprechende Zahl um
+	 *  
+	 * @param c (char) das umzuwandelnde Zeichen
+	 * @return (int) das umgewandelte Zeichen
+	 */
 	public static int inZahl(char c){
 
 		int i = (int) (c - 55);
@@ -121,7 +232,12 @@ public class NumeralSystem {
 
 	}
 
-
+	/**
+	 * Wandelt eine Zahl größer 9 in das entsprechende Zeichen um
+	 * 
+	 * @param i (int) die umzuwandelnde Zahl
+	 * @return (String) die umgewandelte Zahl
+	 */
 	public static String inZeichen(int i) {
 
 		i = i - 10;
@@ -130,8 +246,5 @@ public class NumeralSystem {
 
 		return ch+"";
 	}
-
-
-
 
 }
--- Namensschema/src/de/unistuttgart/iaas/pse/ex07/p2/test.java
+++ Namensschema/src/de/unistuttgart/iaas/pse/ex07/p2/test.java
@@ -3,11 +3,14 @@ package de.unistuttgart.iaas.pse.ex07.p2;
 public class test {
 
 	public static void main(String[] args) {
-		char c = '6';
-		String s = "6";
-		
-		System.out.println();
+
+		for(int i = -2000; i < 2000; i++){
+			for(int in = -2000; in < 2000; in++){
+				for(int it = -2000; in < 2000; i++){
+					System.out.println(NumeralSystem.zuweisung(i + "", in, it));
+
+				}
+			}
+		}
 	}
-
-
 }
